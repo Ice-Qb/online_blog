8bcb6cd781ef632d6068df4ba77b097743618e4e
onasanderr <onasanderr@gmail.com>
2016-05-11 02:36:33 -0500

    Add pagination, search and filtering by tags and categories

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'puma'
 gem 'acts_as_tree'
+gem 'will_paginate', '~> 3.0.5'
+gem 'acts-as-taggable-on', '~> 3.4'
 
 group :development, :test do
   gem 'byebug'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,28 @@
 class Post < ActiveRecord::Base
   belongs_to :category
+  acts_as_taggable
 
   validates :category, presence: true
   validates :title, presence: true
   validates :body, presence: true
+
+  def self.search(search)
+    if search
+      where('title LIKE ? OR body LIKE ?', "%#{search}%", "%#{search}%")
+    else
+      all
+    end
+  end
+
+  def self.category(category_id)
+    category_id ? where(category_id: category_id.to_i) : all
+  end
+
+  def self.tagged_with(tag)
+    tag ? super : all
+  end
+
+  def tag_list
+    super.join(', ')
+  end
 end
