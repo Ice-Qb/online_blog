95838fa2e4e0ed1973e58b86d43a46f3097db8f0
onasanderr <onasanderr@gmail.com>
2016-05-14 04:55:35 -0500

    Add user, category and posts seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+user = User.new(email: 'john@doe.com', password: '12345678')
+user.confirm!
+user.save!
+user.add_role :admin
+
+root = Category.create(name: "All")
+root.children.create(name: "Real")
+root.children.create(name: "Unreal")
+
+20.times do
+  Post.create!(title: Faker::Book.title, body: Faker::Hipster.paragraph,
+    category_id: (1..3).to_a.sample)
+end
